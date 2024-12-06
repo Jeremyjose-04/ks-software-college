bc7fa859aa0b42e12612040f44ab569a8469268d
Jeremyjose-04 <josejeremy04@gmail.com>
2024-12-06 11:37:44 +0530

    v1 push

---
+++ class1.py
@@ -0,0 +1,6 @@
+a=int(input("enter value for a "));
+b=int(input("enter value for b "));
+c=int(input("enter value for c "));
+x=int(input("enter value for x "));
+y = (a * x**2) + (b * x) + c
+print(y);
